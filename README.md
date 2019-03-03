90780e5c5f1602acc9bea7dc15b2a6f5da1d2c2c
Felix De Montis <felix@demont.is>
2019-03-03 18:31:50 +0100

    save data from trello

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Website for Martina / [@tini95](https://www.instagram.com/tini95/).
 
 Site uses [Trello](https://trello.com) as CMS and
 [Cloudinary](https://cloudinary.com/) to resize the images.
+
+Content from Trello gets saved into a `data.json` file, via a
+[Webhook](https://developers.trello.com/page/webhooks) to
+[`data.php'](data.php).
---
+++ data.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('config.php');
+$t = $config['trello'];
+
+$host = 'https://api.trello.com';
+$endpoint = '/1/lists/' . $t['list'] . '/cards';
+$params = [
+  'fields' => 'name',
+  'customFieldItems' => true,
+  'attachments' => true,
+  'attachment_fields' => 'url',
+  'key' => $t['key'],
+  'token' => $t['token']
+];
+
+$url = $host . $endpoint . '?' . http_build_query($params);
+
+$links_response = file_get_contents($url);
+$links_response = json_decode($links_response, true);
+
+$links = [];
+foreach ($links_response as $link_r) {
+
+  $link = [
+    'name' => $link_r['name']
+  ];
+
+  if (count($attachments = $link_r['attachments'])) {
+    $link['image'] = $link_r['attachments'][0]['url'];
+  }
+
+  if (count($customFieldItems = $link_r['customFieldItems'])) {
+    foreach ($customFieldItems as $field) {
+      $fieldId = $field['idCustomField'];
+
+      switch ($fieldId) {
+        case '5c6405c00456d7824f5c2fe9':
+          $link['link'] = $field['value']['text'];
+          break;
+        case '5c6405acfbb82540c2ff100c':
+          $link['description'] = $field['value']['text'];
+          break;
+      }
+    }
+  }
+
+  array_push($links, $link);
+}
+
+file_put_contents('data.json', json_encode($links));
+
+?>
